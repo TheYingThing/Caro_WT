c22b3b7c1a29dceb4e9980ae0c0aa629c4aedfb3
TheYingThing <yingling.dang@gmail.com>
2022-10-30 17:50:11 +0100

    fixed deprecated syntax

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,29 @@
+const droppabletiles = document.getElementsByClassName("drop-spot");
+const tiles = document.getElementsByClassName("player-tile");
 
-
-function allowDrop(ev) {
+let allowDrop = function(ev) {
     ev.preventDefault();
-}
+};
 
-function drag(ev) {
+let drop = function(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    document.getElementById(data).classList.add("set-tile");
+};
+
+let drag = function(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    document.getElementById(data).classList.add("set-tile");
+for (let i = 0; i < droppabletiles.length ; i++) {
+    droppabletiles[i].addEventListener('drop', drop, false)
+    droppabletiles[i].addEventListener('dragover', allowDrop, false)
 }
+
+for (let i = 0; i < tiles.length ; i++) {
+    tiles[i].addEventListener('dragstart', drag, false)
+}
+
+
+

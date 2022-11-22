ab29b3612df1ec2a9401aac06a0e659024302a6a
TheYingThing <yingling.dang@gmail.com>
2022-11-22 09:26:47 +0100

    tile put via ajax wip

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,4 @@
 const droppabletiles = document.getElementsByClassName("drop-spot");
-const tiles = document.getElementsByClassName("player-tile");
-const navLink = document.querySelectorAll(".nav-link");
 const menuButtons = document.getElementsByClassName("menu-button");
 const dropdownTiles = document.getElementsByClassName("dropdown-tile");
 
@@ -22,27 +20,14 @@ let putTileOnly = async function(ev) {
     let row = menu.getAttribute("data-row");
     let col = menu.getAttribute("data-col");
     let color = ev.target.parentElement.getAttribute("data-color");
-    let result = await putOnly(row, col, color);
+    let path = "putOnly/" + row + "/" + col + "/" + color;
+    let result = await executeAjax(path);
     console.log(result);
+    console.log(document)
     document.getElementById("tile" + row + col).firstElementChild.src = "/assets/images/" + result['color'] + "Button.png";
 
 }
-let drop = async function(ev) {
-    console.log(ev.target.getAttribute("data-row"));
-    let row = ev.target.getAttribute("data-row");
-    let col = ev.target.getAttribute("data-column");
-    let tileID = ev.dataTransfer.getData("text");
-    let color = document.getElementById(tileID).getAttribute("data-color");
-    let valid = await checkRules(row, col, color)
-    $.ajax({
-        url:'/put/' + row + '/' + col + '/' + color,
-        type: 'GET'
-    });
-};
 
-let drag = function(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
 
 for (let i = 0; i < dropdownTiles.length ; i++) {
     dropdownTiles[i].addEventListener('click', putTileOnly, false);
@@ -72,7 +57,7 @@ async function startGame()  {
     } else {
         player2 = elements.item(1).value;
     }
-    const path = '/game/' + p1 + '/' + p2;
+    const path = '/game/' + player1 + '/' + player2;
     await executeAjax(path);
     window.location.href = "http://localhost:9000/board";
 }

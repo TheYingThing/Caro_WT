830c821cbc08cb0481bf13b261630cfa43811ae7
TheYingThing <yingling.dang@gmail.com>
2022-12-18 12:02:18 +0100

    complete mess, but the framework stands

---
+++ caro-vue/src/components/Tile.vue
@@ -0,0 +1,22 @@
+<template>
+  <img v-if="color === 'none'" class="tile-img" src="@/assets/images/noTile.png" alt="no tile">
+  <img v-else-if="color === 'red'" class="tile-img" src="@/assets/images/redButton.png" alt="red tile">
+  <img v-else-if="color === 'black'" class="tile-img" src="@/assets/images/blackButton.png" alt="black tile">
+  <img v-else-if="color === 'grey'" class="tile-img" src="@/assets/images/greyButton.png" alt="grey tile">
+  <img v-else-if="color === 'white'" class="tile-img" src="@/assets/images/whiteButton.png" alt="white tile">
+</template>
+
+<script>
+export default {
+  name: 'Tile',
+  props: ['color'],
+}
+</script>
+
+<style>
+.tile-img {
+  width: 30px;
+  height: 30px;
+}
+</style>
+
